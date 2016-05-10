39be13adfebda26fb3015996cee80470850bd773
fleaf5 <dongzli@hotmail.com>
2016-05-10 11:11:38 -0400

    5.9 start change abstract

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
 git commit -m "5.9 start change abstract"
 
-git remote add 5.9 https://github.com/fleaf5/ksz_21cm_1.git`
+git remote add 5.9 https://github.com/fleaf5/ksz_21cm_1.git
 git push -u 5.9 master
 
