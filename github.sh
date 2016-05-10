7b1ddc70d0467e1c4636785adabbbcd18352d601
fleaf5 <dongzli@hotmail.com>
2016-05-10 11:10:29 -0400

    5.9 start change abstract

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "5.5 no part, no rsd, with statistical error"
+git commit -m "5.9 start change abstract"
 
-git remote add 5.5 https://github.com/fleaf5/tidal-ksz.git
-git push -u 5.5 master
+git remote add 5.9 https://github.com/fleaf5/tidal-ksz_1.git
+git push -u 5.9 master
 
