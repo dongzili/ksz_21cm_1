cb96032f52d48868b2175af775b4e0f43191dbe0
fleaf5 <dongzli@hotmail.com>
2016-05-12 22:52:07 -0400

    5.12 new style

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "5.9.1 start change abstract"
+git commit -m "5.12 new style"
 
-git remote add 5.9.1 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 5.9.1 master
+git remote add 5.12 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 5.12 master
 
