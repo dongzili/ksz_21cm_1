68d7f349e4c63669b6ef10fb839dc4660195c0fd
fleaf5 <dongzli@hotmail.com>
2016-05-13 23:04:51 -0400

    5.13 version2 first draft

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "5.12 new style"
+git commit -m "5.13 version2 first draft"
 
-git remote add 5.12 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 5.12 master
+git remote add 5.13 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 5.13 master
 
