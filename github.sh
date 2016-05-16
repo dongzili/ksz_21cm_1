62ce7ede103b33eba426ebbdf0ac2531944ab5db
fleaf5 <dongzli@hotmail.com>
2016-05-16 10:22:26 -0400

    5.14 version2 second draft

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "5.13 version2 first draft"
+git commit -m "5.14 version2 second draft"
 
-git remote add 5.13 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 5.13 master
+git remote add 5.14 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 5.14 master
 
