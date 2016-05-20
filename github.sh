f68f5d0cdde4f24f5206cea771d28a3a11712b38
fleaf5 <dongzli@hotmail.com>
2016-05-20 14:57:21 -0400

    5.20 add rsd and control group explanation

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "5.15 version2 second draft"
+git commit -m "5.20 add rsd and control group explanation"
 
-git remote add 5.15 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 5.15 master
+git remote add 5.20 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 5.20 master
 
