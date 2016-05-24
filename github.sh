1f2fc90496c1ef71e6a7abf86eec947298d7d3f9
fleaf5 <dongzli@hotmail.com>
2016-05-24 10:25:34 -0400

    5.23 minor revise

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "5.20 add rsd and control group explanation"
+git commit -m "5.23 minor revise"
 
-git remote add 5.20 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 5.20 master
+git remote add 5.23 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 5.23 master
 
