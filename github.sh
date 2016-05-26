d78d084d53ce9a63fe2ba43373472ab3d23f0a8f
fleaf5 <dongzli@hotmail.com>
2016-05-26 09:30:04 -0400

    5.26.1 just for aesthetic

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "5.26 for boring aesthetic"
+git commit -m "5.26.1 just for aesthetic"
 
-git remote add 5.26 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 5.26 master
+git remote add 5.26.1 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 5.26.1 master
 
