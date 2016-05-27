7f640adadfb2740cf742eb4df4c87fb389c2d76d
fleaf5 <dongzli@hotmail.com>
2016-05-27 09:01:46 -0400

    5.26.2 cleaner format

---
+++ content/replace.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#sed -i 's/substracted/filtered/g' *.tex
+sed -i 's/21cm/21\ cm/g' *.tex
--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "5.26.1 just for aesthetic"
+git commit -m "5.26.2 cleaner format"
 
-git remote add 5.26.1 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 5.26.1 master
+git remote add 5.26.2 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 5.26.2 master
 
