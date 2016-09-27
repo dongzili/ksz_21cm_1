c13fc138ee7fbaad04a9d385415fc0699af3f008
fleaf5 <dongzli@hotmail.com>
2016-09-26 22:26:02 -0400

    9.26 with plots of 21cm_cmb

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "9.25 with ns_tide"
+git commit -m "9.26 with plots of 21cm_cmb"
 
-git remote add 9.25 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 9.25 master
+git remote add 9.26 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 9.26 master
 
