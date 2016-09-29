97ed3c8ba7b2977e0f00b5b7600449099bdc92d6
fleaf5 <dongzli@hotmail.com>
2016-09-28 21:38:31 -0400

    9.28 accumulative S/N

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "9.27 with new plots cl,sn"
+git commit -m "9.28 accumulative S/N"
 
-git remote add 9.27 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 9.27 master
+git remote add 9.28 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 9.28 master
 
