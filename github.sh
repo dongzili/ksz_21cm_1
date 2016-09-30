48338e6ba8e1e07388bc4dbba9360675717895a6
fleaf5 <dongzli@hotmail.com>
2016-09-30 18:02:33 -0400

    9.30.1 rough total revised1

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "9.30 rough total with abstract"
+git commit -m "9.30.1 rough total revised1"
 
-git remote add 9.30 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 9.30 master
+git remote add 9.30.1 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 9.30.1 master
 
