fee7a61f62e940b6be49cbd449108fbd677eea5b
fleaf5 <dongzli@hotmail.com>
2016-09-30 22:18:11 -0400

    9.30.2 revised2

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "9.30.1 rough total revised1"
+git commit -m "9.30.2 revised2"
 
-git remote add 9.30.1 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 9.30.1 master
+git remote add 9.30.2 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 9.30.2 master
 
