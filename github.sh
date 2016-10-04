4fb450c05e004cb638f7f20834c8bfc23d489775
fleaf5 <dongzli@hotmail.com>
2016-10-03 21:57:00 -0400

    10.3 slight revise, add delta+phase

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 git add *
-git commit -m "9.30.2 revised2"
+git commit -m "10.3 slight revise, add delta+phase"
 
-git remote add 9.30.2 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 9.30.2 master
+git remote add 10.3 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 10.3 master
 
