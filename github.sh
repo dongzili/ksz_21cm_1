683f42b1781994ac8b900d376e23eb92e5ba2f14
fleaf5 <dongzli@hotmail.com>
2016-10-30 12:07:43 -0400

    10.30.1 delete redundant files

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 git rm -r --cached .
 git add .
-git commit -m "10.30 delete redundant files"
+git commit -m "10.30.1 delete redundant files"
 
-git remote add 10.30 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 10.30 master
+git remote add 10.30.1 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 10.30.1 master
 
