00aa7d5ff686321da8e87ae17a9acce8733bbd6c
fleaf5 <dongzli@hotmail.com>
2016-12-10 19:55:47 -0500

    12.10 new draft, needs one more graph

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 git rm -r --cached .
 git add .
-git commit -m "12.9 switch the sequence of kSZ scale explanation"
+git commit -m "12.10 new draft, needs one more graph"
 
-git remote add 12.9 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 12.9 master
+git remote add 12.10 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 12.10 master
 
