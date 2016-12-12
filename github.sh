f3939e664d50135390bee51835ee49bb1ffe4306
fleaf5 <dongzli@hotmail.com>
2016-12-11 21:51:15 -0500

    12.11 new version with figure for p_||

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 git rm -r --cached .
 git add .
-git commit -m "12.10 new draft, needs one more graph"
+git commit -m "12.11 new version with figure for p_||"
 
-git remote add 12.10 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 12.10 master
+git remote add 12.11 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 12.11 master
 
