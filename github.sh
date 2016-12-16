0ad4c5f503187d9c8f1ee7227bf18937dad2d43a
fleaf5 <dongzli@hotmail.com>
2016-12-15 23:01:05 -0500

    12.15 add a figure demonstrate algorithm

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 git rm -r --cached .
 git add .
-git commit -m "12.14 minor change from Mao's suggestion"
+git commit -m "12.15 add a figure demonstrate algorithm"
 
-git remote add 12.14 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 12.14 master
+git remote add 12.15 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 12.15 master
 
