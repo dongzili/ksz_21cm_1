2fa8026ff54ba4a9449902a4a8eddc5bbfbc5bb6
fleaf5 <dongzli@hotmail.com>
2016-12-17 12:40:44 -0500

    12.16 minor change of figure labels

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 git rm -r --cached .
 git add .
-git commit -m "12.15 add a figure demonstrate algorithm"
+git commit -m "12.16 minor change of figure labels"
 
-git remote add 12.15 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 12.15 master
+git remote add 12.16 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 12.16 master
 
