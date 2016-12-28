e563637c28dab0064c21ecec9a267acef1f22f7c
fleaf5 <dongzli@hotmail.com>
2016-12-27 23:24:19 -0500

    12.27.1 revise after phil's comments

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 #git rm -r --cached .
 git add .
-git commit -m "12.27 revise after phil's comments"
+git commit -m "12.27.1 revise after phil's comments"
 
-git remote add 12.27 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 12.27 master
+git remote add 12.27.1 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 12.27.1 master
 
