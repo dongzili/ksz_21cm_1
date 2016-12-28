bb21fd2a02549022887d7ca8f47bea54461f3895
fleaf5 <dongzli@hotmail.com>
2016-12-27 23:14:14 -0500

    12.27 revise after phil's comments

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
-git rm -r --cached .
+#git rm -r --cached .
 git add .
-git commit -m "12.17.1 re-sentencing the first two paragraph of intro"
+git commit -m "12.27 revise after phil's comments"
 
-git remote add 12.17.1 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 12.17.1 master
+git remote add 12.27 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 12.27 master
 
