8b9bc478b00f7992d551896c472290f1ec68ab34
fleaf5 <dongzli@hotmail.com>
2016-12-29 14:09:59 -0500

    12.29 revise several figures

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 #git rm -r --cached .
 git add .
-git commit -m "12.27.2 revise after phil's comments"
+git commit -m "12.29 revise several figures"
 
-git remote add 12.27.2 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 12.27.2 master
+git remote add 12.29 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 12.29 master
 
