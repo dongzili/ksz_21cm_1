b4c925f7a1c4d42fa14ddad1646efd53570a5036
fleaf5 <dongzli@hotmail.com>
2016-12-29 23:43:58 -0500

    12.29.1 incomplete abstract and intro

--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 #git rm -r --cached .
 git add .
-git commit -m "12.29 revise several figures"
+git commit -m "12.29.1 incomplete abstract and intro"
 
-git remote add 12.29 https://github.com/fleaf5/ksz_21cm_1.git
-git push -u 12.29 master
+git remote add 12.29.1 https://github.com/fleaf5/ksz_21cm_1.git
+git push -u 12.29.1 master
 
